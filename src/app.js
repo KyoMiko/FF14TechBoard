2dcf7f7bc91923e63aebcbac74050930e2c02d1e
江峻 <jiangjun9@chinatowercom.cn>
2023-11-09 17:46:41 +0800

    项目初始化

---
+++ src/app.js
@@ -0,0 +1,321 @@
+/****************************************************************************
+ Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
+
+ http://www.cocos2d-x.org
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/
+
+window.game = {}
+window.listener = {}
+window.panel = {}
+window.panel.target = {}
+
+const HelloWorldLayer = cc.Layer.extend({
+    sprite: null,
+    ctor: function () {
+        //////////////////////////////
+        // 1. super init first
+        this._super();
+
+        /////////////////////////////
+        // 2. add a menu item with "X" image, which is clicked to quit the program
+        //    you may modify it.
+        // ask the window size
+        const size = cc.winSize;
+
+        let background = new cc.Sprite(res.background);
+        background.attr({
+            x: size.width / 2,
+            y: size.height / 2
+        })
+        let backgroundSize = background.getBoundingBox();
+        let backgroundScale = Math.min(size.width / backgroundSize.width, size.height / backgroundSize.height);
+        background.setScale(backgroundScale * 0.9)
+        this.addChild(background, -1)
+        backgroundSize = background.getBoundingBox();
+
+        window.panel.target = {};
+        window.game.layer = this;
+        window.game.moveableList = {};
+        window.game.mechanismList = {};
+        window.game.playerList = {};
+
+
+        window.game.iconSize = backgroundSize.width / 30;
+
+        //初始化玩家
+        let tag = Math.floor(Math.random() * 100000);
+        let mt = new cc.Sprite(res.warrior);
+        mt.attr({
+            x: size.width / 2 - game.iconSize * 1.5 - 10,
+            y: size.height / 3
+        })
+        mt.setTag(tag)
+        mt.setUserData({
+            type: "tank"
+        })
+
+        let mtSize = mt.getBoundingBox();
+        let scale = game.iconSize / mtSize.width;
+        mt.setScale(scale)
+
+        this.addChild(mt, 0)
+        game.playerList[tag] = mt
+
+        tag = Math.floor(Math.random() * 100000)
+        let st = new cc.Sprite(res.paladin);
+        st.attr({
+            x: size.width / 2 - 5 - game.iconSize / 2,
+            y: size.height / 3
+        })
+        st.setTag(tag)
+        st.setUserData({
+            type: "tank"
+        })
+        st.setScale(scale)
+        this.addChild(st, 0)
+        game.playerList[tag] = st
+
+        let h1 = new cc.Sprite(res.whitemage);
+        h1.attr({
+            x: size.width / 2 + game.iconSize / 2 + 5,
+            y: size.height / 3
+        })
+        h1.setTag(tag)
+        h1.setUserData({
+            type: "healer"
+        })
+        h1.setScale(scale)
+        this.addChild(h1, 0)
+        game.playerList[tag] = h1
+
+        let h2 = new cc.Sprite(res.scholar);
+        h2.attr({
+            x: size.width / 2 + game.iconSize * 1.5 + 10,
+            y: size.height / 3
+        })
+        h2.setTag(tag)
+        h2.setUserData({
+            type: "healer"
+        })
+        h2.setScale(scale)
+        this.addChild(h2, 0)
+        game.playerList[tag] = h2
+
+        let d1 = new cc.Sprite(res.dragoon);
+        d1.attr({
+            x: size.width / 2 - game.iconSize * 1.5 - 10,
+            y: size.height / 3 - game.iconSize - 5
+        })
+        d1.setTag(tag)
+        d1.setUserData({
+            type: "dps"
+        })
+        d1.setScale(scale)
+        this.addChild(d1, 0)
+        game.playerList[tag] = d1
+
+        let d2 = new cc.Sprite(res.samurai);
+        d2.attr({
+            x: size.width / 2 - 5 - game.iconSize / 2,
+            y: size.height / 3 - game.iconSize - 5
+        })
+        d2.setTag(tag)
+        d2.setUserData({
+            type: "dps"
+        })
+        d2.setScale(scale)
+        this.addChild(d2, 0)
+        game.playerList[tag] = d2
+
+        let d3 = new cc.Sprite(res.bard);
+        d3.attr({
+            x: size.width / 2 + game.iconSize / 2 + 5,
+            y: size.height / 3 - game.iconSize - 5
+        })
+        d3.setTag(tag)
+        d3.setUserData({
+            type: "dps"
+        })
+        d3.setScale(scale)
+        this.addChild(d3, 0)
+        game.playerList[tag] = d3
+
+        let d4 = new cc.Sprite(res.summoner);
+        d4.attr({
+            x: size.width / 2 + game.iconSize * 1.5 + 10,
+            y: size.height / 3 - game.iconSize - 5
+        })
+        d4.setTag(tag)
+        d4.setUserData({
+            type: "dps"
+        })
+        d4.setScale(scale)
+        this.addChild(d4, 0)
+        game.playerList[tag] = d4
+
+        //初始化事件
+        window.listener.touchEmptyListener = cc.EventListener.create({
+            event: cc.EventListener.TOUCH_ONE_BY_ONE,
+            onTouchBegan: function (touch, event) {
+                if (panel.target.type === "new_target") {
+                    const data = panel.target;
+                    const touchLocation = touch.getLocation();
+                    for (const tag in game.moveableList) {
+                        const sprite = game.moveableList[tag];
+                        const locationInNode = sprite.convertToNodeSpace(touchLocation);
+                        const s = sprite.getContentSize()
+                        const rect = cc.rect(0, 0, s.width, s.height)
+                        if (cc.rectContainsPoint(rect, locationInNode)) {
+                            return false;
+                        }
+                    }
+
+                    const tag = Math.floor(Math.random() * 100000)
+                    let target = new cc.Sprite();
+                    target.attr({
+                        x: touchLocation.x,
+                        y: touchLocation.y
+                    })
+                    target.setTag(tag)
+
+                    const icon = data.icon;
+                    if (icon) {
+                        cc.loader.loadImg(icon, {isCrossOrigin: false}, function (err, img) {
+                            const texture = new cc.Texture2D();
+                            texture.initWithElement(img);
+                            texture.handleLoadedTexture();
+                            target.setTexture(texture);
+                        });
+                    } else {
+                        const texture = cc.textureCache.addImage(res.mechanism);
+                        target.setTexture(texture);
+                    }
+
+                    const size = data.size;
+                    if (size) {
+                        target.setScale(game.iconSize * size / 2 / target.getBoundingBox().width)
+                    } else {
+                        target.setScale(game.iconSize / target.getBoundingBox().width)
+                    }
+
+                    game.layer.addChild(target, 0)
+                    cc.eventManager.addListener(listener.moveItemListener.clone(), target)
+                    game.playerList[tag] = target
+                    game.calculateMechanism()
+                }
+            }
+        })
+
+        window.listener.moveItemListener = cc.EventListener.create({
+            event: cc.EventListener.TOUCH_ONE_BY_ONE,
+            swallowTouches: true,
+            onTouchBegan: function (touch, event) {
+                const target = event.getCurrentTarget();
+                const locationInNode = target.convertToNodeSpace(touch.getLocation());
+                const s = target.getContentSize()
+                const rect = cc.rect(0, 0, s.width, s.height)
+
+                window.listener.movingDistance = 0;
+
+                return cc.rectContainsPoint(rect, locationInNode);
+
+            },
+            onTouchMoved: function (touch, event) {
+                const target = event.getCurrentTarget();
+                const delta = touch.getDelta();
+                target.x += delta.x;
+                target.y += delta.y;
+
+                listener.movingDistance += delta.x * delta.x + delta.y * delta.y;
+
+                game.calculateMechanism()
+            },
+            onTouchEnded: function (touch, event) {
+                if (listener.movingDistance < 2) {
+                    if (panel.target.type === "mechanism") {
+                        const target = event.getCurrentTarget();
+                        const tag = Math.floor(Math.random() * 100000)
+                        let mechanism = new cc.Sprite();
+                        mechanism.attr({
+                            x: 0,
+                            y: 0
+                        })
+                        mechanism.setTag(tag)
+                        mechanism.setUserData(panel.target.userData)
+                        target.addChild(mechanism, -1)
+                        game.mechanismList[tag] = mechanism;
+                    }
+                }
+            }
+        })
+
+        cc.eventManager.addListener(listener.touchEmptyListener, background)
+        cc.eventManager.addListener(listener.moveItemListener, mt)
+        cc.eventManager.addListener(listener.moveItemListener.clone(), st)
+        cc.eventManager.addListener(listener.moveItemListener.clone(), h1)
+        cc.eventManager.addListener(listener.moveItemListener.clone(), h2)
+        cc.eventManager.addListener(listener.moveItemListener.clone(), d1)
+        cc.eventManager.addListener(listener.moveItemListener.clone(), d2)
+        cc.eventManager.addListener(listener.moveItemListener.clone(), d3)
+        cc.eventManager.addListener(listener.moveItemListener.clone(), d4)
+
+        return true;
+    }
+});
+
+window.game.calculateMechanism = function () {
+    const playerList = game.playerList;
+    const mechanismList = game.mechanismList;
+    for (const tag in mechanismList) {
+        const mechanism = mechanismList[tag];
+        const data = mechanism.getUserData()
+        if (data.type === 1) {
+            let mechanismLocation = mechanism.getPosition()
+            let minLength = 1000000;
+            let minTag = 0;
+            for (const playerTag in playerList) {
+                const player = playerList[playerTag];
+                const playerLocation = player.getPosition();
+                let x = playerLocation.x - mechanismLocation.x;
+                let y = playerLocation.y - mechanismLocation.y;
+                const dis = Math.sqrt(x * x + y * y)
+                if (dis < minLength) {
+                    minLength = dis;
+                    minTag = playerTag;
+                }
+            }
+            let draw = new cc.DrawNode();
+            let p2 = mechanism.convertToNodeSpace(playerList[minTag].getPosition());
+            draw.drawSegment(p2, cc.p(0, 0), 1, cc.color(255, 255, 255, 255));
+            mechanism.removeAllChildren()
+            mechanism.addChild(draw, 0)
+        }
+    }
+}
+
+const HelloWorldScene = cc.Scene.extend({
+    onEnter: function () {
+        this._super();
+        const layer = new HelloWorldLayer();
+        this.addChild(layer);
+    }
+});
+
---
+++ src/main.js
@@ -0,0 +1,172 @@
+const {createApp} = Vue;
+
+const vm = createApp({
+    data() {
+        return {
+            connectionList: {},
+            host: true,
+            selfNum: "",
+            roomNum: "",
+            ws: null,
+            data: ""
+        }
+    },
+    methods: {
+        createWebSocket() {
+            return new Promise((resolve, reject) => {
+                this.ws = new WebSocket("ws://buaa-jj.cn:3001")
+                this.ws.onmessage = (event) => {
+                    const body = JSON.parse(event.data)
+                    const data = body.data;
+                    switch (body.type) {
+                        case "connect" :
+                            this.selfNum = data.num
+                            resolve()
+                            break;
+                        case "call":
+                            const callFrom = data.from;
+                            const connection = new RTCPeerConnection({
+                                iceServers: [
+                                    {
+                                        urls: "stun:39.104.26.107:3478"
+                                    }
+                                ]
+                            });
+                            connection.ondatachannel = (event) => {
+                                const channel = event.channel;
+                                channel.onmessage = this.handleReceive
+                                this.connectionList[callFrom].channel = channel;
+                            }
+                            connection.onicecandidate = (event) => {
+                                if (event.candidate) {
+                                    this.ws.send(JSON.stringify({
+                                        type: 'ice',
+                                        data: {
+                                            from: this.selfNum,
+                                            to: callFrom,
+                                            candidate: event.candidate
+                                        }
+                                    }))
+                                }
+                            }
+                            this.connectionList[callFrom] = {peer: connection};
+                            connection.setRemoteDescription(data.sdp).then(() => {
+                                connection.createAnswer().then((desc) => {
+                                    connection.setLocalDescription(desc).then(() => {
+                                        this.ws.send(JSON.stringify({
+                                            type: 'answer',
+                                            data: {
+                                                from: this.selfNum,
+                                                to: callFrom,
+                                                sdp: desc
+                                            }
+                                        }))
+                                    })
+                                })
+                            })
+                            break;
+                        case "answer":
+                            const answerFrom = data.from;
+                            if (this.connectionList[answerFrom]) {
+                                const connection = this.connectionList[answerFrom].peer;
+                                connection.setRemoteDescription(data.sdp)
+                            }
+                            break;
+                        case "ice":
+                            const iceFrom = data.from;
+                            if (this.connectionList[iceFrom]) {
+                                const connection = this.connectionList[iceFrom].peer;
+                                connection.addIceCandidate(data.candidate)
+                            }
+                            break;
+                    }
+                }
+                this.ws.onopen = () => {
+                    this.ws.send(JSON.stringify({
+                        type: 'connect'
+                    }))
+                }
+            })
+        },
+        createConnection() {
+            this.closeAllPeer()
+            if (!this.ws) {
+                this.createWebSocket()
+            }
+        },
+        joinConnection() {
+            this.closeAllPeer()
+            if (!this.ws) {
+                this.createWebSocket().then(() => {
+                    this.initPeer()
+                })
+            } else {
+                this.initPeer()
+            }
+        },
+        initPeer() {
+            const roomNum = this.roomNum
+            const peer = new RTCPeerConnection(
+                {
+                    iceServers: [
+                        {
+                            urls: "stun:39.104.26.107:3478"
+                        }
+                    ]
+                }
+            )
+            const channel = peer.createDataChannel("sendChannel");
+            channel.onopen = () => {
+                channel.send("Hello World!")
+            }
+            peer.onicecandidate = (event) => {
+                if (event.candidate) {
+                    this.ws.send(JSON.stringify({
+                        type: 'ice',
+                        data: {
+                            from: this.selfNum,
+                            to: this.roomNum,
+                            candidate: event.candidate
+                        }
+                    }))
+                }
+            }
+            peer.createOffer().then((desc) => {
+                peer.setLocalDescription(desc).then(() => {
+                    this.ws.send(JSON.stringify({
+                        type: 'call',
+                        data: {
+                            from: this.selfNum,
+                            to: roomNum,
+                            sdp: desc
+                        }
+                    }))
+                })
+            })
+            this.connectionList[roomNum] = {
+                peer: peer,
+                channel: channel
+            };
+        },
+        closeAllPeer() {
+            for (const key in this.connectionList) {
+                const connection = this.connectionList[key];
+                connection.channel.close();
+                connection.peer.close();
+            }
+            this.connectionList = {};
+        },
+        sendMessage() {
+            for (const key in this.connectionList) {
+                const connection = this.connectionList[key];
+                connection.channel.send(this.data)
+            }
+        },
+        handleReceive(event) {
+            this.data = event.data;
+        }
+    }
+})
+
+vm.use(ElementPlus)
+vm.mount('#app')
