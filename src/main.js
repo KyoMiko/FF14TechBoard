9eab85dc0221dd40d1dedc524f79a26c07d25db4
江峻 <jiangjun9@chinatowercom.cn>
2023-11-09 18:03:15 +0800

    更新wss

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ const vm = createApp({
     methods: {
         createWebSocket() {
             return new Promise((resolve, reject) => {
-                this.ws = new WebSocket("ws://buaa-jj.cn:3001")
+                this.ws = new WebSocket("wss://buaa-jj.cn/websocket")
                 this.ws.onmessage = (event) => {
                     const body = JSON.parse(event.data)
                     const data = body.data;
